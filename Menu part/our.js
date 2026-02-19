783346c6e024271f3b069bcfe8d50c27e3e37c14
Mudadla kalebu <mudadlakalebu@Mudadlas-MacBook-Air.local>
2026-02-19 23:32:13 +0530

    fix some charges

---
+++ Menu part/our.js
@@ -0,0 +1,14 @@
+function showCategory(category) {
+    let items = document.querySelectorAll('.menu-item');
+    items.forEach(item => {
+        if (category === 'all' || item.classList.contains(category)) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
+function showAlert() {
+    alert("Successfully added to cart!");
+}
---
+++ payment login/a1.js
@@ -0,0 +1,18 @@
+function pay() {
+    const cardNumber = document.getElementById('cardnum').value;
+    const cardHolderName = document.getElementById('holdername').value;
+    const expiry = document.getElementById('expiry').value;
+    const cvv = document.getElementById('cvv').value;
+
+    const paymentData = {
+        cardNumber: cardNumber,
+        cardHolderName: cardHolderName,
+        expiry: expiry,
+        cvv: cvv
+    };
+    
+    console.log('Payment Data:', paymentData);
+    window.location.href="http://127.0.0.1:5500/a2.html";
+}
+
+
